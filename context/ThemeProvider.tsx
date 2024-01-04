36ad8152bb4d68091ede14fdca5938b47e05835f
Arkadiusz <areq12x@gmail.com>
2024-01-04 11:34:28 +0100

    Working on theme switcher with shadcn components. Added constans file and types for constans.

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface ThemeContextType {
   mode: string;
@@ -11,22 +11,22 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mode, setMode] = useState('');
+  const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === 'dark') {
-      setMode('light');
-      document.documentElement.classList.add('light');
+    if (mode === "dark") {
+      setMode("light");
+      document.documentElement.classList.add("light");
     } else {
-      setMode('dark');
-      document.documentElement.classList.add('dark');
+      setMode("dark");
+      document.documentElement.classList.add("dark");
     }
   };
 
   useEffect(() => {
     handleThemeChange();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [mode]);
+  }, []);
 
   return (
     <ThemeContext.Provider
@@ -44,7 +44,7 @@ export function useTheme() {
   const context = useContext(ThemeContext);
 
   if (context === undefined) {
-    throw new Error('useTheme must be used within a ThemeProvider');
+    throw new Error("useTheme must be used within a ThemeProvider");
   }
 
   return context;
