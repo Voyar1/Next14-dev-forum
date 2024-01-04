58cc97aa9ebef2f594ebfe01ed3b2b37983466fa
Arkadiusz <areq12x@gmail.com>
2024-01-05 00:28:12 +0100

    Implemented responsive design for nav links.

--- components/shared/sidebar/Sidebar.tsx
+++ components/shared/sidebar/Sidebar.tsx
@@ -1,29 +1,49 @@
+"use client";
+
 import { sidebarLinks } from "@/constans";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import React from "react";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
-    <aside className="background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col overflow-y-auto p-6 pt-36 shadow-light-300 lg:w-[266px]">
-      <div className="flex flex-1 flex-col items-start gap-6">
-        {sidebarLinks.map((link) => (
-          <Link
-            href={link.route}
-            key={link.route}
-            className="flex-center gap-4"
-          >
-            <Image
-              src={link.imgURL}
-              alt={link.label}
-              width={24}
-              height={24}
-              className="invert-colors"
-            />
-            <p className="text-dark200_light900 text-lg">{link.label}</p>
-          </Link>
-        ))}
+    <aside className="background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col overflow-y-auto p-6 pt-36 shadow-light-300 max-sm:hidden  lg:w-[266px]">
+      <div className="flex flex-1 flex-col gap-4 ">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900 bg-transparent"
+              } text-dark200_light900 flex items-center justify-start gap-4 bg-transparent p-4 `}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p
+                className={`${
+                  isActive ? "text-light-900" : "text-dark300_light900"
+                } base-medium max-lg:hidden`}
+              >
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
       </div>
     </aside>
   );
