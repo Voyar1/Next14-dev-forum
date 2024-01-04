10149c35794a68f8b6f30cea4238abe37cf2d068
Arkadiusz <areq12x@gmail.com>
2024-01-04 13:01:38 +0100

    Installed sheet component from shadcn, implemented mobile nav elements like: -logo -login and sign up button. Implemented styles for mobile nav

---
+++ components/shared/navbar/MobileNav.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
+const NavContent = () => {
+  return <h1>NavContent</h1>;
+};
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="menu"
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="Dev Forum"
+          />
+          <p className="h2-bold text-dark100_light900 font-spaceGrotesk ">
+            Dev <span className="text-primary-500">Flow</span>
+          </p>
+        </Link>
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+        </div>
+        <SignedOut>
+          <div className="flex flex-col gap-3 ">
+            <SheetClose asChild>
+              <Link href="/sign-in">
+                <Button className="small-medium btn-secondary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 font-bold shadow-none">
+                  <span className="primary-text-gradient">Log in</span>
+                </Button>
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link href="/sign-up">
+                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 font-bold shadow-none">
+                  Sign Up
+                </Button>
+              </Link>
+            </SheetClose>
+          </div>
+        </SignedOut>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
