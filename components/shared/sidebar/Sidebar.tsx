d7196d4f8fcce5e65e64e3688cf3daf8b4e2f2f2
Arkadiusz <areq12x@gmail.com>
2024-01-07 13:21:17 +0100

    refactored code of left sidebar

--- components/shared/sidebar/Sidebar.tsx
+++ components/shared/sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+
 import { sidebarLinks } from "@/constans";
 import { SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
@@ -13,35 +14,36 @@ const Sidebar = () => {
   const pathname = usePathname();
 
   return (
-    <aside className="background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col overflow-y-auto p-6 pt-36 shadow-light-300 max-sm:hidden  lg:w-[266px]">
-      <div className="flex flex-1 flex-col gap-4 ">
-        {sidebarLinks.map((link) => {
+    <aside className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen w-fit flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 max-sm:hidden  lg:w-[266px] dark:shadow-none">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((item) => {
           const isActive =
-            (pathname.includes(link.route) && link.route.length > 1) ||
-            pathname === link.route;
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
+
           return (
             <Link
-              href={link.route}
-              key={link.route}
+              href={item.route}
               className={`${
                 isActive
                   ? "primary-gradient rounded-lg text-light-900"
-                  : "text-dark300_light900 bg-transparent"
-              } text-dark200_light900 flex items-center justify-start gap-4 bg-transparent p-4 `}
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+              key={item.route}
             >
               <Image
-                src={link.imgURL}
-                alt={link.label}
-                width={24}
-                height={24}
-                className={`${isActive ? "" : "invert-colors"}`}
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}  `}
               />
               <p
                 className={`${
-                  isActive ? "text-light-900" : "text-dark300_light900"
-                } base-medium max-lg:hidden`}
+                  isActive ? "base-bold" : "base-medium"
+                } max-lg:hidden`}
               >
-                {link.label}
+                {item.label}
               </p>
             </Link>
           );
@@ -51,14 +53,30 @@ const Sidebar = () => {
       <SignedOut>
         <div className="flex flex-col gap-3 ">
           <Link href="/sign-in">
-            <Button className="small-medium btn-secondary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 font-bold shadow-none">
-              <span className="primary-text-gradient">Log in</span>
+            <Button className="small-medium btn-secondary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 font-bold shadow-none ">
+              <Image
+                src="/assets/icons/account.svg"
+                alt="login"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Log in
+              </span>
             </Button>
           </Link>
 
           <Link href="/sign-up">
-            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 font-bold shadow-none">
-              Sign Up
+            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 font-bold shadow-none ">
+              <Image
+                src="/assets/icons/sign-up.svg"
+                alt="signup"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="max-lg:hidden">Sign Up</span>
             </Button>
           </Link>
         </div>
