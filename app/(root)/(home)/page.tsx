6bc31045d1f55599cf087306ee30236dd849fa42
Arkadiusz <areq12x@gmail.com>
2024-01-08 19:15:51 +0100

    Started working on homepage layout. Added local search bar, h1 and button

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,23 @@
+import LocalSearch from "@/components/shared/LocalSearch";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="h-screen"></div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row">
+        <h1 className="h1-bold text-dark100_light900 sm:items-center">
+          All questions
+        </h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch />
+      </div>
+    </>
+  );
 }
