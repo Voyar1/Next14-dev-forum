06b9beba57f395fbbd84861ed339e596a61b6cc6
Arkadiusz <areq12x@gmail.com>
2024-01-09 19:35:34 +0100

    refactored code of Local Search Bar. This component is now customizable by props

--- components/shared/LocalSearch.tsx
+++ components/shared/LocalSearch.tsx
@@ -1,25 +1,53 @@
+"use client";
+
 import React from "react";
 import { Input } from "../ui/input";
 import Image from "next/image";
 
-const LocalSearch = () => {
+interface CustomInputProps {
+  route: string;
+  iconPosition: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+const LocalSearch = ({
+  route,
+  iconPosition,
+  imgSrc,
+  placeholder,
+  otherClasses,
+}: CustomInputProps) => {
   return (
-    <div className="relative w-full">
-      <div className="background-light800_darkgradient relative flex min-h-[56px] items-center gap-1 rounded-xl px-4">
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      {iconPosition === "left" && (
         <Image
-          src="/assets/icons/search.svg"
-          alt="search"
+          src={imgSrc}
+          alt="search icon"
           width={24}
           height={24}
           className="cursor-pointer"
         ></Image>
-        <Input
-          type="text"
-          placeholder="Search questions..."
-          // value=""
-          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
-        />
-      </div>
+      )}
+      <Input
+        type="text"
+        placeholder={`${placeholder}...`}
+        // value=""
+        onChange={() => {}}
+        className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+      />
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          alt="search icon"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        ></Image>
+      )}
     </div>
   );
 };
