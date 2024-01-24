a843d39bc6a03b0dfad17fd0a02d9b16cf878f2b
Arkadiusz <areq12x@gmail.com>
2024-01-24 18:21:24 +0100

    Added funcionality to createQuestion action function which handles adding tags to database

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,10 +1,38 @@
 "use server";
 
+import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
 
 export async function createQuestion(params: any) {
   // eslint-disable-next-line no-empty
   try {
     connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+    // Create question
+
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagDocuments = [];
+
+    // Create the tags or get them if they already exist
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        {
+          name: {
+            $regex: new RegExp(`^${tag}$`, "i"),
+          },
+        },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true }
+      );
+
+      tagDocuments.push(existingTag._id);
+    }
   } catch (error) {}
 }
