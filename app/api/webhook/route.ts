b55998fabcdcee4804fe18a90f453a43f897fb64
Arkadiusz <areq12x@gmail.com>
2024-02-07 19:07:33 +0100

    fixed naming in route.ts file

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,6 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  //   TODO: ADD WEBHOOK TO ENVLOCAL
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -33,7 +32,7 @@ export async function POST(req: Request) {
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
+  // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
@@ -52,11 +51,7 @@ export async function POST(req: Request) {
     });
   }
 
-  // Get the ID and type
-
   const eventType = evt.type;
-
-  console.log({ eventType });
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
@@ -95,10 +90,13 @@ export async function POST(req: Request) {
 
   if (eventType === "user.deleted") {
     const { id } = evt.data;
-    const deletedUser = await deleteUser({ clerkId: id! });
+
+    const deletedUser = await deleteUser({
+      clerkId: id!,
+    });
 
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  return new Response("", { status: 200 });
+  return NextResponse.json({ message: "OK" });
 }
