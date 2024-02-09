196073a5b57aad29922a78de2ed4e383b747c9e1
Arkadiusz <areq12x@gmail.com>
2024-02-09 11:29:24 +0100

    Implemented real data userId in ask-question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 import Question from "@/components/forms/Question";
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { getUserById } from "@/lib/actions/user.action";
 
 const Page = async () => {
-  // const { userId } = auth();
-
-  const userId = "CL123456";
+  const { userId } = auth();
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
