ca136327b670cd6e0d9902ca9e2ecb93d9269050
Arkadiusz <areq12x@gmail.com>
2024-02-12 18:38:17 +0100

    Finished layout of community page, created UserCard component

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,9 +1,13 @@
+import UserCard from "@/components/cards/UserCard";
 import Filter from "@/components/shared/Filter";
 import LocalSearch from "@/components/shared/LocalSearch";
 import { UserFilters } from "@/constans/filters";
+import { getAllUsers } from "@/lib/actions/user.action";
+import Link from "next/link";
 import React from "react";
 
-const page = () => {
+const Page = async () => {
+  const result = await getAllUsers({});
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row">
@@ -24,9 +28,28 @@ const page = () => {
           otherClasses="min-h-[56px] sm:min-w-[170px]"
         />
       </div>
-      <section className="mt-12 flex flex-wrap gap-4"></section>
+      <section className="mt-12 flex flex-wrap gap-4">
+        {result.users.length > 0 ? (
+          result.users.map((user) => (
+            <UserCard
+              key={user._id}
+              user={user}
+            />
+          ))
+        ) : (
+          <div className="paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center">
+            <p>No users yet</p>
+            <Link
+              href="/sign-up"
+              className="mt-1 font-bold text-accent-blue"
+            >
+              Join to be the first!
+            </Link>
+          </div>
+        )}
+      </section>
     </>
   );
 };
 
-export default page;
+export default Page;
