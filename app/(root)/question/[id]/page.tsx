4b98bfbe211f7ded5671e83dc0d69603018eb77a
Arkadiusz <areq12x@gmail.com>
2024-02-16 17:43:36 +0100

    Created layout for user information and voting section

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,7 +1,34 @@
+import { getQuestionById } from "@/lib/actions/question.action";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Page = () => {
-  return <div>Page</div>;
+const Page = async ({ params }) => {
+  const result = await getQuestionById({ questionId: params.id });
+  return (
+    <>
+      <div className="flex-start w-full flex-col">
+        <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+          <Link
+            href={`/profile/${result.author.clerkId}`}
+            className="flex items-center justify-start gap-1"
+          >
+            <Image
+              src={result.author.picture}
+              className="rounded-full"
+              width={22}
+              height={22}
+              alt="profile"
+            />
+            <p className="paragraph-semibold text-dark300_light700">
+              {result.author.name}
+            </p>
+          </Link>
+          <div className="flex justify-end">Voting</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Page;
