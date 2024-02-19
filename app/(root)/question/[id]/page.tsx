a4ca844c18a5045bcdae3401de9de248ce7a149f
Arkadiusz <areq12x@gmail.com>
2024-02-19 17:55:16 +0100

    Added types for next.js params

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Page = async ({ params }) => {
+const Page = async ({ params }: { params: { id: string } }) => {
   const result = await getQuestionById({ questionId: params.id });
   return (
     <>
