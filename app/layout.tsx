ea61636358f764a05574dd8a011f04b165d3dfdf
Arkadiusz <areq12x@gmail.com>
2024-02-20 17:47:47 +0100

    Installed Prism to handle code blocks display. Added styles for prism.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider } from "@clerk/nextjs";
 // eslint-disable-next-line camelcase
-import { Inter, Space_Grotesk } from 'next/font/google';
-import React from 'react';
-import './globals.css';
-import type { Metadata } from 'next';
-import { ThemeProvider } from '@/context/ThemeProvider';
+import { Inter, Space_Grotesk } from "next/font/google";
+import React from "react";
+import "./globals.css";
+import "../styles/prism.css";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter',
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
 });
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-spaceGrotesk',
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
 });
 
 export const metadata: Metadata = {
-  title: 'Next.js 14 with Clerk',
+  title: "Next.js 14 with Clerk",
   description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore eaque eum placeat incidunt. Ipsam similique sed ad eius magnam?',
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore eaque eum placeat incidunt. Ipsam similique sed ad eius magnam?",
   icons: {
-    icon: '/assets/images/site-logo.svg',
+    icon: "/assets/images/site-logo.svg",
   },
 };
 
@@ -33,13 +34,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
         <ClerkProvider
           appearance={{
             elements: {
-              formButtonPrimary: 'primary-gradient',
-              footerActionLink: 'primary-text-gradient hover:text-primary-500',
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
             },
           }}
         >
