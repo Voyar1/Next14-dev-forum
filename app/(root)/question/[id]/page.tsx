ef53594a338ba8f65b062457fb45e1cacb940f44
Arkadiusz <areq12x@gmail.com>
2024-02-21 17:31:10 +0100

    Created answer form component

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Answer from "@/components/forms/Answer";
 import Metric from "@/components/shared/Metric";
 import ParseHTML from "@/components/shared/ParseHTML";
 import RenderTag from "@/components/shared/RenderTag";
@@ -68,6 +69,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
           />
         ))}
       </div>
+      <Answer />
     </>
   );
 };
