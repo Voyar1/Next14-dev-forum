4741a4c142e4b19acc61cd3264318f57ebd72fa5
Arkadiusz <areq12x@gmail.com>
2024-02-22 17:58:27 +0100

    Finished the answer form

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -1,7 +1,105 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { AnswerSchema } from "@/lib/validations";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Editor } from "@tinymce/tinymce-react";
+import { useTheme } from "@/context/ThemeProvider";
+import { Button } from "../ui/button";
 
 const Answer = () => {
-  return <div>Answer</div>;
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const editorRef = useRef(null);
+  const { mode } = useTheme();
+  const form = useForm<z.infer<typeof AnswerSchema>>({
+    resolver: zodResolver(AnswerSchema),
+    defaultValues: {
+      answer: "",
+    },
+  });
+
+  const handleCreateAnswer = (data) => {};
+
+  return (
+    <Form {...form}>
+      <form
+        className="mt-6 flex w-full flex-col gap-10"
+        onSubmit={form.handleSubmit(handleCreateAnswer)}
+      >
+        <FormField
+          control={form.control}
+          name="answer"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormControl className=" mt-3.5">
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor;
+                  }}
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "code",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                      "codesample",
+                    ],
+                    toolbar:
+                      "undo redo | blocks | " +
+                      "codesample bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist outdent indent | " +
+                      "removeformat | help",
+                    content_style:
+                      "body { font-family:Inter,sans-serif; font-size:16px }",
+                    skin: mode === "dark" ? "oxide-dark" : "oxide",
+                    content_css: mode === "dark" ? "dark" : "light",
+                  }}
+                />
+              </FormControl>
+
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-end">
+          <Button
+            type="submit"
+            className="primary-gradient w-fit text-white"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
 };
 
 export default Answer;
