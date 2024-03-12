25800e821d62080758b82e130c156eff22aee094
Arkadiusz <areq12x@gmail.com>
2024-03-12 20:16:55 +0100

    Implemented full functionality of answer voting

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -93,7 +93,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
       </div>
       <AllAnswers
         questionId={result._id}
-        userId={JSON.stringify(mongoUser._id)}
+        userId={mongoUser._id}
         totalAnswers={result.answers.length}
       />
       <Answer
