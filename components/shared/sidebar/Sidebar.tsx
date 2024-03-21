3cbec585e165d90a15620e3e2c86b0c2b4504df4
Arkadiusz <areq12x@gmail.com>
2024-03-21 17:56:07 +0100

    Fixed bug with wrong pathname to profile page

--- components/shared/sidebar/Sidebar.tsx
+++ components/shared/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 
 import { sidebarLinks } from "@/constans";
-import { SignedOut } from "@clerk/nextjs";
+import { SignedOut, useAuth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -11,6 +11,7 @@ import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
+  const { userId } = useAuth();
   const pathname = usePathname();
 
   return (
@@ -20,6 +21,14 @@ const Sidebar = () => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
             pathname === item.route;
+
+          if (item.route === "/profile") {
+            if (userId) {
+              item.route = `${item.route}/${userId}`;
+            } else {
+              return null;
+            }
+          }
 
           return (
             <Link
