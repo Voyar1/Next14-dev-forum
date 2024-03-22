20210b3bed25f496e121092440dce1e8f7d17ef2
Arkadiusz <areq12x@gmail.com>
2024-03-22 18:08:33 +0100

    Created basic layout for profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getUserInfo } from "@/lib/actions/user.action";
 import { URLProps } from "@/types";
+import Image from "next/image";
 import React from "react";
 
 const Page = async ({ params, searchParams }: URLProps) => {
@@ -7,7 +8,21 @@ const Page = async ({ params, searchParams }: URLProps) => {
 
   return (
     <>
-      <div className="flex flex-col-reverse items-start justify-between sm:flex-row"></div>
+      <div className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <Image
+            src={userInfo?.user.picture}
+            alt="profile picture"
+            width={140}
+            height={140}
+            className="rounded-full object-cover"
+          />
+          <div className="mt-3">
+            <h2>{userInfo.user.name}</h2>
+            <p>@{userInfo.user.username}</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
