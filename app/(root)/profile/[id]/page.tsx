6bd0360efe3343da94927b0f8b424a77b13acf1a
Arkadiusz <areq12x@gmail.com>
2024-03-23 18:04:40 +0100

    Created action function for getting all user info

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,8 +1,15 @@
+import { getUserInfo } from "@/lib/actions/user.action";
 import { URLProps } from "@/types";
 import React from "react";
 
-const Page = ({ params, searchParams }): URLProps => {
-  return <div>{params.id}</div>;
+const Page = async ({ params, searchParams }: URLProps) => {
+  const userInfo = await getUserInfo({ userId: params.id });
+
+  return (
+    <>
+      <div className="flex flex-col-reverse items-start justify-between sm:flex-row"></div>
+    </>
+  );
 };
 
 export default Page;
