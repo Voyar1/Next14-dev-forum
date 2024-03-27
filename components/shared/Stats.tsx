db3a02af85c4816d8d45031cead3586907be3221
Arkadiusz <areq12x@gmail.com>
2024-03-27 19:09:38 +0100

    Created cards for badges

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -1,5 +1,25 @@
 import { formatNumberWithExtension } from "@/lib/utils";
+import Image from "next/image";
 import React from "react";
+
+interface StatsCardProps {
+  imgUrl: string;
+  value: number;
+  title: string;
+}
+
+const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => {
+  return (
+    <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
+      <Image src={imgUrl} alt={title} width={40} height={50} />
+      <div>
+        <p className="paragraph-semibold text-dark200_light900">{value}</p>
+        <p className="body-medium text-dark400_light700">{title}</p>
+      </div>
+    </div>
+  );
+};
+
 interface Props {
   totalQuestions: number;
   totalAnswers: number;
@@ -23,6 +43,21 @@ const Stats = ({ totalQuestions, totalAnswers }: Props) => {
             <p className="body-medium text-dark400_light700">Answers</p>
           </div>
         </div>
+        <StatsCard
+          imgUrl="/assets/icons/gold-medal.svg"
+          value={0}
+          title="Gold Badges"
+        />
+        <StatsCard
+          imgUrl="/assets/icons/silver-medal.svg"
+          value={0}
+          title="Silver Badges"
+        />
+        <StatsCard
+          imgUrl="/assets/icons/bronze-medal.svg"
+          value={0}
+          title="Bronze Badges"
+        />
       </div>
     </div>
   );
