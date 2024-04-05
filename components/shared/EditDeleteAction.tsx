0314a22ef2d664764c8d650ad7629c8d2544a68c
Arkadiusz <areq12x@gmail.com>
2024-04-05 11:36:55 +0200

    Crated layout for Edit functionality

--- components/shared/EditDeleteAction.tsx
+++ components/shared/EditDeleteAction.tsx
@@ -1,10 +1,27 @@
+"use client";
+import Image from "next/image";
 import React from "react";
 interface Props {
   type: string;
   itemId: string;
 }
 const EditDeleteAction = ({ type, itemId }: Props) => {
-  return <div>EditDeleteAction</div>;
+  const handleEdit = () => {};
+  const handleDelete = () => {};
+  return (
+    <div className="flex items-center justify-end gap-3 max-sm:w-full">
+      {type === "Question" && (
+        <Image
+          src="/assets/icons/edit.svg"
+          alt="Edit"
+          width={14}
+          height={14}
+          className="cursor-pointer object-contain"
+          onClick={handleEdit}
+        />
+      )}
+    </div>
+  );
 };
 
 export default EditDeleteAction;
