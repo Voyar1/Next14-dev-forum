1cf1abc910074bb23ba370c5554e81b23c27e9cd
Arkadiusz <areq12x@gmail.com>
2024-04-09 20:15:47 +0200

    Created layout of edit question page

--- app/(root)/question/edit/[id]/page.tsx
+++ app/(root)/question/edit/[id]/page.tsx
@@ -1,7 +1,15 @@
+import Question from "@/components/forms/Question";
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Edit Question</h1>
+      <div className="mt-9">
+        <Question type="edit" mongoUserId="" questionDetails="" />
+      </div>
+    </>
+  );
 };
 
 export default Page;
