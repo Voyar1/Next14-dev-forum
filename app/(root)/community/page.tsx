4f572510400a5475038d0466e09c63f4126496f9
Arkadiusz <areq12x@gmail.com>
2024-04-29 18:22:11 +0200

    Implemented search functionality on community page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -3,11 +3,14 @@ import Filter from "@/components/shared/Filter";
 import LocalSearch from "@/components/shared/LocalSearch";
 import { UserFilters } from "@/constans/filters";
 import { getAllUsers } from "@/lib/actions/user.action";
+import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 import React from "react";
 
-const Page = async () => {
-  const result = await getAllUsers({});
+const Page = async ({ searchParams }: SearchParamsProps) => {
+  const result = await getAllUsers({
+    searchQuery: searchParams.q,
+  });
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row">
@@ -30,19 +33,11 @@ const Page = async () => {
       </div>
       <section className="mt-12 flex flex-wrap gap-4">
         {result.users.length > 0 ? (
-          result.users.map((user) => (
-            <UserCard
-              key={user._id}
-              user={user}
-            />
-          ))
+          result.users.map((user) => <UserCard key={user._id} user={user} />)
         ) : (
           <div className="paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center">
             <p>No users yet</p>
-            <Link
-              href="/sign-up"
-              className="mt-1 font-bold text-accent-blue"
-            >
+            <Link href="/sign-up" className="mt-1 font-bold text-accent-blue">
               Join to be the first!
             </Link>
           </div>
