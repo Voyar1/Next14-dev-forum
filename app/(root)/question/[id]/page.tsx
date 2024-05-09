cb58ec46a82d130e68f000a65d1120e5603eac6d
Arkadiusz <areq12x@gmail.com>
2024-05-09 18:48:53 +0200

    Implemented sorting for answers

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -12,7 +12,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Page = async ({ params }: { params: { id: string } }) => {
+const Page = async ({ params, searchParams }: { params: { id: string } }) => {
   const { userId: clerkId } = auth();
   let mongoUser;
 
@@ -95,6 +95,8 @@ const Page = async ({ params }: { params: { id: string } }) => {
         questionId={result._id}
         userId={mongoUser._id}
         totalAnswers={result.answers.length}
+        page={searchParams.page}
+        filter={searchParams.filter}
       />
       <Answer
         question={result.content}
